f4ea6c8eb5a55b8a7ea03065f415e7d531adc995
andrewdc92 <andrew.cordivari@gmail.com>
2017-02-10 22:55:32 -0800

    adds reading and writing to json functionality, adds note validation

---
+++ playground/json.js
@@ -0,0 +1,24 @@
+// var obj = {
+//   name: 'Andrew'
+// };
+// var stringObj = JSON.stringify(obj);
+// console.log(typeof stringObj);
+// console.log(stringObj);
+
+// var personString = '{"name": "Andrew", "age": 24}';
+// var person = JSON.parse(personString);
+
+const fs = require('fs');
+
+var originalNote = {
+  title: 'Some title',
+  body: 'Some content'
+};
+var originalNoteString = JSON.stringify(originalNote);
+
+fs.writeFileSync('notes.json', originalNoteString)
+
+var noteString = fs.readFileSync('notes.json');
+var note = JSON.parse(noteString);
+console.log(typeof note);
+console.log(note.title);
